904e7fa200146e41e7d3ec5f8107a1c0189e11bc
foques <rhuanwinicyusk@gmail.com>
2026-01-01 17:18:07 -0300

    initial

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar const img
+  const img = document.querySelector("#profile img")
+  //se tiver light mode adicionar a imagem light
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("scr", "./assets/avatar")
+  }
+
+  // pegar a tag img
+
+  //se tiver light mode adicionar a imagem light
+  //substituir a imagem
+  //se tiver sem light mode manter a imagem normal
+}
